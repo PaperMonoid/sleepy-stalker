97926f634e2a70c1bfd33713af1198f574432f99
TritiumMonoid <tritiummonoid@gmail.com>
2019-01-12 00:17:27 -0800

    fix server to work with databse

--- server/main.py
+++ server/main.py
@@ -1,12 +1,13 @@
 import mysql.connector
 from connection import config
 from flask import Flask, request
+from datetime import datetime
 
 app = Flask("sleepy-stalker")
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/up", methods=["GET"])
+def up():
+    return "", 200
 
 @app.route("/wakeup", methods=["GET", "POST"])
 def wakeup():
@@ -16,15 +17,13 @@ def wakeup():
         try:
             connection = mysql.connector.connect(**config)
             cursor = connection.cursor()
-            query = ("SELECT SleepId FROM Sleep ORDER BY SleepTime DESC LIMIT 1")
-            cursor.execute(query)
-            sleep = cursor.fetchone()
             add_wake_up = ("INSERT INTO WakeUp"
-                           "(SleepId, WakeUpType)"
-                           "VALUES(%(id)s, %(type)s)")
+                           "(WakeUpTime, WakeUpReason)"
+                           "VALUES(%(time)s, %(reason)s)")
             data_wake_up = {
-                "id": sleep[0],
-                "type": request.json.get("type")
+                "time": (request.json.get("time") or
+                         datetime.now().strftime("%Y-%m-%d %H:%M:%S")),
+                "reason": request.json.get("reason") or "natural"
             }
             cursor.execute(add_wake_up, data_wake_up)
             connection.commit()
@@ -43,9 +42,16 @@ def sleep():
             connection = mysql.connector.connect(**config)
             cursor = connection.cursor()
             add_sleep = ("INSERT INTO Sleep"
-                         "(Food, Exercise, Stress, Mood)"
-                         "VALUES(%(food)s, %(exercise)s, %(stress)s, %(mood)s)")
-            data_sleep = request.json
+                         "(SleepTime, Food, Exercise, Stress, Mood)"
+                         "VALUES(%(time)s, %(food)s, %(exercise)s, %(stress)s, %(mood)s)")
+            data_sleep = {
+                "time": (request.json.get("time") or
+                         datetime.now().strftime("%Y-%m-%d %H:%M:%S")),
+                "food": request.json.get("food") or "low",
+                "exercise": request.json.get("exercise") or "low",
+                "stress": request.json.get("stress") or "low",
+                "mood": request.json.get("mood") or "happy"
+            }
             cursor.execute(add_sleep, data_sleep)
             connection.commit()
             cursor.close()
